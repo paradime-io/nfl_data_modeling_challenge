d838a0585373180b282ed7a77d438d28a5bc3140
Shyam Sunder <shyamns.279@gmail.com>
2025-01-11 13:35:46 +0000

    Revert changes made to model name

--- models/intermediate/int_fantasy_points.sql
+++ models/intermediate/int_fantasy_points.sql
@@ -12,7 +12,7 @@ SELECT
     ROUND(SUM(fantasy_points) / COUNT(DISTINCT season_week), 2) AS avg_fantasy_points_per_game,
     ROUND(SUM(fantasy_points_ppr) / COUNT(DISTINCT season_week), 2) AS avg_fantasy_points_ppr_per_game
 FROM 
-    {{ ref('stg_player_stats_by_game_2023') }}
+    {{ ref('stg_player_stats_by_game') }}
 WHERE 
     SEASON_TYPE = 'REG'
 GROUP BY 
