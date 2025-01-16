9e693c9f7c88f3e43cae07ee99021db22b01227d
Katie Shaffer <katieeaton5@gmail.com>
2025-01-16 04:33:27 +0000

    Fix for fct_fantasy_point_leaders totals

--- models/marts/facts/fct_fantasy_point_leaders.sql
+++ models/marts/facts/fct_fantasy_point_leaders.sql
@@ -3,14 +3,15 @@ SELECT
     player_name,
     position,
     recent_team,
-    total_fantasy_points,
-    total_fantasy_points_ppr,
-    games_played,
-    avg_fantasy_points_per_game,
-    avg_fantasy_points_ppr_per_game,
-    RANK() OVER (ORDER BY total_fantasy_points DESC) AS standard_rank,
-    RANK() OVER (ORDER BY total_fantasy_points_ppr DESC) AS ppr_rank
+    SUM(total_fantasy_points) as total_fantasy_points,
+    SUM(total_fantasy_points_ppr) as total_fantasy_points_ppr,
+    --  games_played,
+    --  avg_fantasy_points_per_game,
+    --  avg_fantasy_points_ppr_per_game,
+    --  RANK() OVER (ORDER BY total_fantasy_points DESC) AS standard_rank,
+    --  RANK() OVER (ORDER BY total_fantasy_points_ppr DESC) AS ppr_rank
 FROM 
     {{ ref('int_fantasy_points') }}
+GROUP BY ALL
 ORDER BY 
     total_fantasy_points DESC
