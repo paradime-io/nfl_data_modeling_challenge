fcd52ffd04afe72c5452d67b7e64555f136dfe6c
Viktor Likár <viktor.likar@smart24.hu>
2025-02-04 23:59:01 +0000

    avg modified in win_prob

--- models/marts/facts/fct_win_prob.sql
+++ models/marts/facts/fct_win_prob.sql
@@ -1,6 +1,7 @@
 select 
     floor(3600 - game_seconds_remaining / 60) * 60 as game_time,
-    winning_team_wp,
-    1-winning_team_wp as losing_team_wp
+    avg(winning_team_wp) as winning_team_wp,
+    1-avg(winning_team_wp) as losing_team_wp
 from {{ref('int_win_prob')}}
 where game_time is not null
+group by 1
