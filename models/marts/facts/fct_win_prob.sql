6fce27469649456cbab1ff0d805fcd1341c15c86
Viktor Likár <viktor.likar@smart24.hu>
2025-02-05 00:00:26 +0000

    gametime fixed

--- models/marts/facts/fct_win_prob.sql
+++ models/marts/facts/fct_win_prob.sql
@@ -1,5 +1,5 @@
 select 
-    floor(3600 - game_seconds_remaining / 60) * 60 as game_time,
+    3600 - floor(game_seconds_remaining / 60) * 60 as game_time,
     avg(winning_team_wp) as winning_team_wp,
     1-avg(winning_team_wp) as losing_team_wp
 from {{ref('int_win_prob')}}
